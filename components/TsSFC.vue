2c5f07b00b14a0b7564b91ae58fd3d961ab0f410
Chris Vaillancourt <cvaillan22@gmail.com>
2021-04-07 16:29:19 -0700

    add example components

---
+++ components/TsSFC.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>result: {{ result }}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data: () => ({
+    a: 1,
+    b: 2,
+  }),
+  computed: {
+    result() {
+      return this.add(this.a, this.b)
+    },
+  },
+  methods: {
+    add(a, b) {
+      return a + b
+    },
+  },
+})
+</script>
---
+++ components/VanillaSFC.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>result: {{ result }}</div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    a: 1,
+    b: 2,
+  }),
+  computed: {
+    result() {
+      return this.add(this.a, this.b)
+    },
+  },
+  methods: {
+    add(a, b) {
+      return a + b
+    },
+  },
+}
+</script>
